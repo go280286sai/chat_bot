bd67cf64d3632685ff93de0982de2d4b859dfac0
Aleksandr <go280286sai@gmail.com>
2025-08-08 15:31:00 +0300

    create database

--- alembic/versions/c08fbabf5417_initial_schema.py
+++ alembic/versions/c08fbabf5417_initial_schema.py
@@ -1,8 +1,8 @@
 """Initial schema
 
-Revision ID: 5c23e8449a8c
+Revision ID: c08fbabf5417
 Revises: 
-Create Date: 2025-08-08 15:08:25.381855
+Create Date: 2025-08-08 15:18:36.326998
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '5c23e8449a8c'
+revision: str = 'c08fbabf5417'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
